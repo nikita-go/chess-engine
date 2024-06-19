e3dcffc597b233f11b6a0ce0670e1f99745f22b4
Nikita Gorshkov <gorshkovnikita@gmail.com>
2024-06-19 21:03:07 +0800

    make game somewhat functional

---
+++ Engine.py
@@ -0,0 +1,53 @@
+# engine
+
+class GameState():
+    def __init__(self):
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp","bp", "bp"],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            ["wp", "wp", "wp", "wp", "wp", "wp","wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
+        ]
+        self.whiteToMove = True
+        self.previousMoves = []
+
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "."
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.previousMoves.append(move)
+        self.whiteToMove = not self.whiteToMove
+
+    def undoMove(self):
+        if len(self.previousMoves)!=0:
+            move = self.previousMoves.pop()
+            self.board[move.startRow][move.startCol] = move.pieceMoved
+            self.board[move.endRow][move.endCol] = move.pieceCaptured
+            self.whiteToMove = not self.whiteToMove
+
+
+
+class Move():
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4, 
+                   "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3,
+                   "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, start, end, board):
+        self.startRow = start[0]
+        self.startCol = start[1]
+        self.endRow = end[0]
+        self.endCol = end[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol)+self.getRankFile(self.endRow, self.endCol)
+
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c]+self.rowsToRanks[r]
---
+++ Main.py
@@ -0,0 +1,77 @@
+# actual game
+
+import pygame as p
+import Engine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+CELL_SIZE = 64
+MAX_FPS = 15
+IMAGES = {}
+
+def loadImages():
+    pieces = ["bR", "bN", "bB", "bQ", "bK", "bp", "wR", "wN", "wB", "wQ", "wK", "wB", "wp"]
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (CELL_SIZE, CELL_SIZE))
+    
+def main():
+    p.init()
+
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gameState = Engine.GameState()
+
+    loadImages()
+
+    running = True
+    cellSelected = ()
+    playerClicks = []
+
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                col = location[0]//CELL_SIZE
+                row = location[1]//CELL_SIZE
+                if cellSelected == (row, col):
+                    cellSelected = ()
+                    playerClicks = []
+                else:
+                    cellSelected = (row, col)
+                    playerClicks.append(cellSelected)
+                if len(playerClicks)==2:
+                    move = Engine.Move(playerClicks[0], playerClicks[1], gameState.board)
+                    gameState.makeMove(move)
+                    print(move.getChessNotation())
+                    cellSelected = ()
+                    playerClicks = []
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z:
+                    gameState.undoMove()
+        drawGameState(screen, gameState)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+def drawGameState(screen, gameState):
+    drawBoard(screen)
+    drawPieces(screen, gameState.board)
+
+def drawBoard(screen):
+    colors = [p.Color(238,238,212,255), p.Color(125,148,92,255)]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[(r+c)%2]
+            p.draw.rect(screen, color, p.Rect(c*CELL_SIZE, r*CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece!=".":
+                screen.blit(IMAGES[piece], p.Rect(c*CELL_SIZE, r*CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
+if __name__=="__main__":
+    main()
